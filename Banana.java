78a8e0ad7db47905ba3041c0f89de0f778480812
Adeline Lai <adelinelai08@gmail.com>
2025-11-10 14:24:32 -0500

    Updated Banana class

--- Banana.java
+++ Banana.java
@@ -1,19 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Banana here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Banana extends Actor
 {
-    /**
-     * Act - do whatever the Banana wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
-        // Add your action code here.
+        move(-10);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+        
+        if(isTouching(Hero.class)){
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    
+    public void resetBanana() {
+        int number = Greenfoot.getRandomNumber(2);
+        if(number == 0) {
+           setLocation(600, 100); 
+        } else {
+            setLocation(600, 300);
+        }
+        
     }
 }
