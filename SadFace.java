386d3ba5c1ea1d3e82a33ac9032a24a65d6501c2
Adeline Lai <adelinelai08@gmail.com>
2025-11-11 13:58:05 -0500

    Updated SadFace

--- SadFace.java
+++ SadFace.java
@@ -12,6 +12,9 @@ public class SadFace extends Actor
      * Act - do whatever the SadFace wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public SadFace() {
+        setImage("images/smiley3.png");
+    }
     public void act()
     {
         // Add your action code here.
